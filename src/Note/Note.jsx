77c3d9989f377c545fb655b52ccd25e9b1932db6
Gabriel Pigatto <gabrielpigatto@live.com>
2018-09-14 12:10:21 -0300

    Update Note.jsx

--- src/Note/Note.jsx
+++ src/Note/Note.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
- 
+import Grid from '@material-ui/core/Grid/Grid';
+import Card from '@material-ui/core/Card/Card';
+import { CardContent, CardActions} from '@material-ui/core';
+import Button from '@material-ui/core/Button/Button';
+import Check from '@material-ui/icons/Check';
+import Typography from '@material-ui/core/Typography/Typography';
+import Divider from '@material-ui/core/Divider/Divider';
+
+const cardStyle = {
+    padding: 20,
+    margin: 20,
+    marginTop: 0,
+    marginBottom: 0,
+  };
+  
+const root = {
+  flexGrow: 1,
+}
+
 class Note extends Component {
 
     constructor (props){
@@ -16,12 +34,25 @@ class Note extends Component {
 
     render(props){
         return(
-            <div>
-                
-                <p>{this.noteContent}
-                <span onClick={() => this.handleRemoveNote(this.noteId)}> X</span>
-                </p>
-            </div>
+            <div className = {root}>
+            <Grid container direction = "column" alignItems="strech" spacing = {8}>
+              <Grid item xs>
+                <Card style = {cardStyle}>
+                  <CardContent>
+                    <Typography variant="body1" color="textPrimary">
+                        {this.noteContent}
+                    </Typography>  
+                  </CardContent>
+                  <CardActions>
+                      <Button color="primary" variant="extendedFab" onClick={() => this.handleRemoveNote(this.noteId)}>
+                      DONE!
+                      <Check style={{marginLeft: 5,fontSize: 20,}}/>
+                      </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            </Grid>
+          </div>   
         );
     }
 }
