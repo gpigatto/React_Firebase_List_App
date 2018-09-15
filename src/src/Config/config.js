99a3a43b6cfb3c3c6fd836e8101c2eed5d2c357c
Gabriel Pigatto <gabrielpigatto@live.com>
2018-09-15 10:30:01 -0300

    Delete config.js

--- src/src/Config/config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyDJknezKXL_kH0a0SZqJBw5IlRbDI6nJas",
-    authDomain: "simple-notes-be438.firebaseapp.com",
-    databaseURL: "https://simple-notes-be438.firebaseio.com",
-    projectId: "simple-notes-be438",
-    storageBucket: "simple-notes-be438.appspot.com",
-    messagingSenderId: "1004208100718"
-  };
